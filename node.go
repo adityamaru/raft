1b3e5f20b70c527fffc4991024136a5a54daac83
Aditya Maru <adityamaru@gmail.com>
2020-04-27 17:12:36 -0400

    election timer logic has been added

--- node.go
+++ node.go
@@ -2,7 +2,9 @@ package raft
 
 import (
 	"log"
+	"math/rand"
 	"sync"
+	"time"
 )
 
 // LogEntry represents an entry in the nodes log.
@@ -44,6 +46,8 @@ type Node struct {
 	participantNodes []int
 
 	state nodeState
+
+	timeSinceTillLastReset time.Time
 
 	server *Server
 
@@ -87,7 +91,8 @@ func (node *Node) RequestVote(args RequestVoteArgs, reply *RequestVoteReply) err
 
 	log.Print("RequestVote args: %+v\ncurrentTerm=%d\nvotedFor=%d", args, node.currentTerm, node.votedFor)
 
-	// If the RPC term is less than the current term then we must reject the vote request.
+	// If the RPC term is less than the current term then we must reject the
+	// vote request.
 	if args.term < node.currentTerm {
 		reply.term = node.currentTerm
 		reply.voteGranted = false
@@ -100,11 +105,12 @@ func (node *Node) RequestVote(args RequestVoteArgs, reply *RequestVoteReply) err
 		node.updateStateToFollower(args.term)
 	}
 
-	// If the above condition was not true then we have to ensure that we have not voted for some other node with the same term.
+	// If the above condition was not true then we have to ensure that we have
+	// not voted for some other node with the same term.
 	if args.term == node.currentTerm && (node.votedFor == -1 || node.votedFor == args.candidateID) {
 		reply.voteGranted = true
 		node.votedFor = args.candidateID
-		// reset the election timer
+		node.timeSinceTillLastReset = time.Now()
 	} else {
 		reply.voteGranted = false
 	}
@@ -113,12 +119,76 @@ func (node *Node) RequestVote(args RequestVoteArgs, reply *RequestVoteReply) err
 	return nil
 }
 
-// This method is responsible for resetting the nodes state to follower.
-// The mutex mu must be held before invoking this method.
+// This method is responsible for resetting the nodes state to follower. The
+// mutex mu must be held before invoking this method.
 func (node *Node) updateStateToFollower(latestTerm int) {
 	node.currentTerm = latestTerm
 	node.state = follower
 	node.votedFor = -1
 
 	// Reset and restart the election timer.
+	node.timeSinceTillLastReset = time.Now()
+
+	// Start the followers election timer concurrently.
+	go node.startElectionTimer()
+}
+
+// Returns a pseudo random duration which is used as the electionTimeout for
+// this node. The range of the duration is [150, 300] as specified in the paper.
+func (node *Node) randElectionTimeout() time.Duration {
+	return time.Duration(150+rand.Intn(150)) * time.Millisecond
+}
+
+// This method is resposnible for periodically checking if a new election is to
+// be started by the node.
+func (node *Node) startElectionTimer() {
+	electionTimeout := node.randElectionTimeout()
+
+	node.mu.Lock()
+	timerStartTerm := node.currentTerm
+	node.mu.Unlock()
+
+	ticker := time.NewTicker(10 * time.Millisecond)
+	defer ticker.Stop()
+
+	// This loops wakes every 10ms and checks if the conditions are conducive
+	// for starting an election. This is not the most efficient and
+	// theoretically we could just wake up every electionTimeout, but this
+	// reduces testability/log readability.
+	for {
+		<-ticker.C
+
+		node.mu.Lock()
+		if node.state != candidate || node.state != follower {
+			log.Print("The node is in the %s state, no need to run election", node.state)
+			node.mu.Unlock()
+			return
+		}
+
+		// Some logic about the timer having been started in a previous term.
+
+		// Run an election if we have reached the election timeout.
+		if timePassed := time.Since(node.timeSinceTillLastReset); timePassed > electionTimeout {
+			node.runElection()
+			node.mu.Unlock()
+			return
+		}
+
+		node.mu.Unlock()
+	}
+}
+
+// Converts a follower to a candidate and starts an election.
+// Assumes mu is held when this method is invoked.
+func (node *Node) runElection() {
+	node.currentTerm++
+	node.state = candidate
+	node.votedFor = node.id
+	node.timeSinceTillLastReset = time.Now()
+
+	log.Print("Node %d has become a candidate with currentTerm=%d", node.id, node.currentTerm)
+
+	votesReceived := 0
+
+	// Sen
 }
--- server.go
+++ server.go
@@ -1,8 +1,42 @@
 package raft
 
-// Server is a wrapper which contains a `node` and an `rpc.Server`. It is responsible for managing RPC communications between `nodes`.
+import (
+	"net/rpc"
+	"sync"
+)
+
+// Server is a wrapper which contains a `node` and an `rpc.Server`. It is
+// responsible for managing RPC communications between `nodes`.
 type Server struct {
-	nodeID int
+	nodeID           int
 	participantNodes []int
+
+	node      *Node
+	rpcServer *rpc.Server
+
+	// This channel is used to indicate to the node that it has been connected
+	// to all the other participants in the raft group.
+	connectedToAllParticipants <-chan interface{}
+
+	// Map from nodeID to that nodes `client` we can Call methods on.
+	nodeIDToClient map[int]*rpc.Client
+
+	mu sync.Mutex
+}
+
+// CreateNewServer returns a server with the provided nodeID and other fields
+// initialized.
+func CreateNewServer(nodeID int, participantNodes []int, connectedToAllParticipants <-chan interface{}) *Server {
+	server := new(Server)
+	server.nodeID = nodeID
+	server.participantNodes = participantNodes
+	server.connectedToAllParticipants = connectedToAllParticipants
+	server.nodeIDToClient = make(map[int]*rpc.Client)
+
+	return server
+}
+
+// StartServer is responsible for
+func (server *Server) StartServer() {
 
 }
